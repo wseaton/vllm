cd17a0af3946084004bb4818de6cdea12c1a4955
Will Eaton <weaton@redhat.com>
2025-05-30 08:43:43 -0400

    checkpoint
    Signed-off-by: Will Eaton <weaton@redhat.com>

--- vllm/transformers_utils/tokenizers/native_tokenizer_ext/src/lib.rs
+++ vllm/transformers_utils/tokenizers/native_tokenizer_ext/src/lib.rs
@@ -1,13 +1,27 @@
+use ndarray::Array;
 use pyo3::prelude::*;
+
+use tokenizers_python::tokenizer::PyTokenizer;
+use tokenizers::Tokenizer;
+use numpy::ndarray::{ArrayD, ArrayViewD, ArrayViewMutD};
+use numpy::{IntoPyArray, PyArrayDyn, PyReadonlyArrayDyn, PyArrayMethods};
+
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn encode<'py>(_py: Python<'py>, tokenizer: &PyTokenizer, text: String) -> PyResult<String> {
+    let t = tokenizer.tokenizer.clone();
+    let result = t.encode(text, false).map_err(|e| {
+        pyo3::exceptions::PyValueError::new_err(format!(
+            "Failed to encode text: {}",
+            e
+        ))
+    }).map(|encoding| encoding.get_tokens().join(" "));
+    Ok(result?)
 }
 
 #[pymodule]
 fn native_tokenizer_ext(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(encode, m)?)?;
     Ok(())
 }
