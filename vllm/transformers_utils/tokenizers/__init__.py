22e4c323d4c408e1f939a74c7fe15e2b182f3f3e
Will Eaton <weaton@redhat.com>
2025-06-02 13:59:03 -0400

    bump

--- vllm/transformers_utils/tokenizers/__init__.py
+++ vllm/transformers_utils/tokenizers/__init__.py
@@ -2,9 +2,9 @@
 
 from .mistral import (MistralTokenizer, maybe_serialize_tool_calls,
                       truncate_tool_call_ids, validate_request_params)
-from .native_tokenizer_ext import sum_as_string
+
 
 __all__ = [
     "MistralTokenizer", "maybe_serialize_tool_calls", "truncate_tool_call_ids",
-    "validate_request_params", "sum_as_string"
+    "validate_request_params", 
 ]
